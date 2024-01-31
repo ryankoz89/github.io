d1e6247cece6a54928a3fca727f7ac8238e00f69
Ryan Kozlowski <ryankoz@yahoo.com>
2024-01-31 00:46:40 -0800

    add times

--- Descending Dates.js
+++ Descending Dates.js
@@ -1,7 +1,9 @@
-const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922, 1989, 2017, 1942, 2023, 2055, 2051, 1986]
+const times = [20, 21, 16, 14, 15]
 
-const sortYears = arr => arr.sort((a, b) => b - a);
+const sortStuff = arr => arr.sort((a, b) => b - a);
 
 
 // Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
-console.log(sortYears(years));
+console.log(sortStuff(years));
+console.log(sortStuff(times));
